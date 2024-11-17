9a4ddf4ad76f6d444dcad1fdf2402bf85ee4b22b
Samyappa P <samyever2be@gmail.com>
2024-11-17 09:32:58 +0530

    second commit

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -3,9 +3,13 @@ import { Link } from 'react-router-dom';
 const Nav = ()=>{
     return(
         <>
-            <Link to="/">Home</Link>
-            <Link to="/demoform">Demo Form</Link>
-            <Link to="/advice">Advice</Link>
+            <div className='navbar'>
+                <div className='navbar-wrapper'>
+                    <Link to="/">Home</Link>
+                    <Link to="/demoform">Demo Form</Link>
+                    <Link to="/advice">Advice</Link>
+                </div>
+            </div>
         </>
     )
 }
